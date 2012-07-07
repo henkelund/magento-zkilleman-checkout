e02b833777f5c83b5f6ccd9f47d8c464fecbb4e5
Henrik Hedelund <henke.hedelund@gmail.com>
2012-07-07 13:31:03 +0200

    Basic module setup

---
+++ app/code/community/Zkilleman/Checkout/Model/Config.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Zkilleman_Checkout
+ *
+ * Copyright (C) 2012 Henrik Hedelund (henke.hedelund@gmail.com)
+ *
+ * This file is part of Zkilleman_Checkout.
+ *
+ * Zkilleman_Checkout is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Zkilleman_Checkout is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Zkilleman_Checkout. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category  Zkilleman
+ * @package   Zkilleman_Checkout
+ * @author    Henrik Hedelund <henke.hedelund@gmail.com>
+ * @copyright 2012 Henrik Hedelund (henke.hedelund@gmail.com)
+ * @license   http://www.gnu.org/licenses/lgpl.html GNU LGPL
+ * @link      https://github.com/henkelund/magento-zkilleman-checkout
+ */
+
+class Zkilleman_Checkout_Model_Config
+{
+    const XML_PATH_CHECKOUT_ENABLED = 'zkilleman_checkout/general/enabled';
+
+    /**
+     * Is Zkilleman_Checkout enabled?
+     *
+     * @return bool
+     */
+    public function isEnabled()
+    {
+        return Mage::getStoreConfigFlag(self::XML_PATH_CHECKOUT_ENABLED);
+    }
+}
